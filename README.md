02f48bade7d9ef85556b8b9b4c328f764afffc33
Kapandron <kapandrondr@yandex.by>
2022-01-13 16:48:46 +0300

    Init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Gradle Using Custom Plugin
+
+- [Link to cusom gradle plugin:](https://jfrog.com/community/open-source)
---
+++ src/main/java/com/clevertec/plugin/using/Runner.java
@@ -0,0 +1,8 @@
+package com.clevertec.plugin.using;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        System.out.println("Just a stub main method");
+    }
+}
